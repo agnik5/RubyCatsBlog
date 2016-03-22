7302639fa62c36497d16e3197d6320abc2477ed1
agnik <reira@wp.pl>
2016-03-22 15:20:40 +0000

    validation and if method in posts create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,24 @@
 class PostsController < ApplicationController
+	
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
 			@post = Post.new(post_params)
-			@post.save
-
+			
+		if @post.save
 			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
-
 		@post = Post.find(params[:id])
 	end
 
